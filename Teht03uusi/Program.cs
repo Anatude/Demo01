586f8a1284ecb558ab7a8b64245c8e20d2e7b9b1
K9456 <K9456@OHJE-330-WS-29>
2017-01-19 14:22:12 +0200

    Add project files.

---
+++ Teht03uusi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht03uusi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double luku1, luku2, luku3, sum, avrg;
+            Console.WriteLine("Anna kolme lukua:  ");
+            luku1 = double.Parse(Console.ReadLine());
+            luku2 = double.Parse(Console.ReadLine());
+            luku3 = double.Parse(Console.ReadLine());
+            sum = (luku1 + luku2 + luku3);
+            avrg = (sum / 3.0);
+            Console.WriteLine("summa on " + sum);
+            Console.WriteLine("Keskiarvo on " + avrg);
+        }
+    }
+}
---
+++ Teht04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Anna ikäsi: ");
+            age = int.Parse(Console.ReadLine());
+
+            if (age >=0 && age <= 17)
+            {
+                Console.WriteLine("Alaikäinen");
+            }
+            if (age >= 18 && age <= 65)
+            {
+                Console.WriteLine("Aikuinen");
+            }
+            if (age >= 66)
+            {
+                Console.WriteLine("Seniori");
+            }
+
+        }
+    }
+}
